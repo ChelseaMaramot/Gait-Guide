d514682a700081256cb378674dc86eaa48d6dbdd
ChelseaMaramot <maramotc@mcmaster.ca>
2022-02-24 19:41:56 -0500

    Added communication functionality between two esp 32

--- rom1/rom1.ino
+++ rom1/rom1.ino
@@ -1,11 +1,60 @@
+//src from https://randomnerdtutorials.com/esp-now-many-to-one-esp32/
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
 
 #define BNO055_SAMPLERATE_DELAY_MS (100)
   
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+// This is the receiver ESP32 --> Master 1420 usb left
+
+
+// Must match the sender structure
+typedef struct struct_message {
+  int id;
+  int x;
+  int y;
+  int z;
+}struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// Create a structure to hold the readings from each board
+struct_message board1;
+
+// Create an array with all the structures
+struct_message boardsStruct[1] = {board1};
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac_addr, const uint8_t *incomingData, int len) {
+
+  // get board macAdress
+  char macStr[18];
+  Serial.print("Packet received from: ");
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.println(macStr);
+
+  
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.printf("Board ID %u: %u bytes\n", myData.id, len);
+  // Update the structures with the new incoming data
+  boardsStruct[myData.id-1].x = myData.x;
+  boardsStruct[myData.id-1].y = myData.y;
+  boardsStruct[myData.id-1].z = myData.z;
+  Serial.printf("x value: %d \n", boardsStruct[myData.id-1].x);
+  Serial.printf("y value: %d \n", boardsStruct[myData.id-1].y);
+  Serial.printf("z value: %d \n", boardsStruct[myData.id-1].z);
+  Serial.println();
+}
+
 
 void displaySensorDetails(void)
 {
@@ -21,6 +70,7 @@ void displaySensorDetails(void)
   Serial.println("------------------------------------");
   Serial.println("");
   delay(500);
+  
 }
 
 void displaySensorStatus(void)
@@ -40,6 +90,7 @@ void displaySensorStatus(void)
   Serial.println(system_error, HEX);
   Serial.println("");
   delay(100);
+
 }
 
 void setup() {
@@ -62,48 +113,34 @@ if(!bno.begin())
   displaySensorStatus();
 
   bno.setExtCrystalUse(true);
+  
+  Serial.println(WiFi.macAddress());
+  //D8:A0:1D:5F:B6:FC
+
+
+  //Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  //Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+  
 }
 
 void loop() {
-  // Possible vector values can be:
-  // - VECTOR_ACCELEROMETER - m/s^2
-  // - VECTOR_MAGNETOMETER  - uT
-  // - VECTOR_GYROSCOPE     - rad/s
-  // - VECTOR_EULER         - degrees
-  // - VECTOR_LINEARACCEL   - m/s^2
-  // - VECTOR_GRAVITY       - m/s^2
-  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  imu::Vector<3> accel_value = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
   
-  /* Display the floating point data */
-  Serial.println("IMU Gyro Data:");
-  Serial.print("X: ");
-  Serial.print(euler.x());
-  Serial.print(" Y: ");
-  Serial.print(euler.y());
-  Serial.print(" Z: ");
-  Serial.print(euler.z());
-  Serial.print("\t\t");
+  // Access the variables for each board
+  int board1X = boardsStruct[0].x;
+  int board1Y = boardsStruct[0].y;
+  int board1Z = boardsStruct[0].z;
 
-  /* Display calibration status for each sensor. */
-  uint8_t system, gyro, accel, mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-  Serial.print("CALIBRATION: Sys=");
-  Serial.print(system, DEC);
-  Serial.print(" Gyro=");
-  Serial.print(gyro, DEC);
-  Serial.print(" Accel=");
-  Serial.print(accel, DEC);
-  Serial.print(" Mag=");
-  Serial.println(mag, DEC);
-
-  Serial.println("IMU Accelerometer Data:");
-  Serial.print("X: ");
-  Serial.print(accel_value.x());
-  Serial.print(" Y: ");
-  Serial.print(accel_value.y());
-  Serial.print(" Z: ");
-  Serial.println(accel_value.z());
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
+  delay(1000);
 }
+
+
---
+++ rom2/rom2.ino
@@ -0,0 +1,151 @@
+//src from https://randomnerdtutorials.com/esp-now-many-to-one-esp32/
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <WiFi.h>
+#include <esp_now.h>
+#define BNO055_SAMPLERATE_DELAY_MS (100)
+
+// This is the sender ESP32 --> Slave 1410 usb right
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+// Contains receiver macAdress
+uint8_t broadcastAddress[] = {0xD8, 0xA0, 0x1D, 0x5F, 0xB6, 0xFC};
+
+typedef struct struct_message{
+  int id;
+  int x;
+  int y;
+  int z;
+} struct_message;
+
+// create a struct_message called myData
+struct_message myData;
+
+//Create peer interface
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  bno.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+  
+}
+
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  Serial.println("");
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
+  Serial.println("");
+  delay(100);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+Serial.println("Orientation Sensor Test"); Serial.println("");
+Wire.begin(); 
+if(!bno.begin())
+{
+  /* There was a problem detecting the BNO055 ... check your connections */
+  Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+  while(1);
+}
+  delay(1000);
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+
+  /* Optional: Display current status */
+  displaySensorStatus();
+
+  bno.setExtCrystalUse(true);
+  
+  Serial.println(WiFi.macAddress());
+  //98:CD:AC:F7:8A:D0
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  
+  // Add peer        
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  Serial.println("Done Setup");
+  
+}
+
+
+
+void loop() {
+
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    
+  // Set values to send
+  myData.id = 1;
+  myData.x = euler.x();
+  myData.y = euler.y();
+  myData.z = euler.z();
+
+  Serial.println(myData.z);
+  
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(1000);
+  
+}
+
+
